c4e53d70bd448af860e95bbbb584f665c0a797f7
ping9pang <155457421+ping9pang@users.noreply.github.com>
2024-12-23 14:41:44 +0900

    sujung

--- js/background.js
+++ js/background.js
@@ -1,13 +1,15 @@
 const bakimages = [
-    "haho.jpeg",
+    "haho.webp",
     "jy.jpeg",
-    "bk.jpeg"
+    "pug.jpeg",
 ];
 
+console.log(Math.floor(Math.random() * bakimages.length))
 const choiceImage = bakimages[Math.floor(Math.random() * bakimages.length)];
 
-const bgImage = document.createElement("bakimages");
+const bgImage = document.createElement("img");
+// background의 이미지를 바꾼다?
 
-bgImage.src = `bakimges/${choiceImage}`;  
+bgImage.src = `img/${choiceImage}`  
 
 document.body.appendChild(bgImage); //html에 선언안해도 선언을 해준다.
